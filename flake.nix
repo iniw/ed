382c4a2c6de4afe471e8f560018de64cb7a48c2d
Vinicius Deolindo <git@vini.cat>
2025-09-21 18:42:42 -0300

    Add a CI

--- flake.nix
+++ flake.nix
@@ -10,17 +10,58 @@
       system:
       let
         pkgs = import inputs.nixpkgs { inherit system; };
+
+        rustToolchain = with pkgs; [
+          cargo
+          clippy
+          rust-analyzer
+          rustc
+          rustfmt
+        ];
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            cargo
-            clippy
-            rust-analyzer
-            rustc
-            rustfmt
-          ];
+          packages = rustToolchain;
         };
+
+        # FIXME: Add tests
+        checks =
+          let
+            check =
+              {
+                name,
+                command,
+                packages,
+              }:
+              {
+                ${name} = pkgs.stdenv.mkDerivation {
+                  inherit name;
+                  src = inputs.self;
+
+                  nativeBuildInputs = packages;
+
+                  buildPhase = ''
+                    ${command}
+                    touch $out
+                  '';
+                };
+              };
+          in
+          check {
+            name = "typos";
+            command = "typos";
+            packages = [ pkgs.typos ];
+          }
+          // check {
+            name = "format";
+            command = "cargo fmt --check";
+            packages = rustToolchain;
+          }
+          // check {
+            name = "lint";
+            command = "cargo clippy -- -Dwarnings";
+            packages = rustToolchain;
+          };
       }
     );
 }
